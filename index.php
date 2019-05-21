e6781795de74584ac7d3cb8a1a94687e8e2efd44
Larry Rose <larrydrose@gmail.com>
2019-05-21 13:24:18 -0400

    This is a test of the first commit after making a change

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 //echo "Hello World";
-$hello = "This is the variable hello";
-$A="hello";
-$B=$$A;
-$printline='$A = '.$A. ' $B = '. $B;
-echo $printline;
-echo $B;
+
+echo "This is a test of the connection to github";
 
 ?>
